9af1b7b8fc7db2cf4afcde8930d04af7784e2119
mogwai <vmichel@intechinfo.fr>
2020-06-09 19:30:21 +0200

    eroor fixing

--- reverse-shell/server-reverse-ssh.py
+++ reverse-shell/server-reverse-ssh.py
@@ -21,10 +21,10 @@ class Server(paramiko.ServerInterface):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((127.0.0.1, 5002))
+    s.bind(('127.0.0.1', 5002))
     s.listen()
     print('Listening')
-    client, addr = sock.accept()
+    client, addr = s.accept()
 
 with paramiko.Transport(client) as t:
     t.load_server_moduli()
